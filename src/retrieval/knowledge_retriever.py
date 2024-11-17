dba62f414ac47009a5c2cef17f73567784a22d56
mfshiu <avatar.xu@gmail.com>
2024-11-18 04:28:58 +0800

    remove _log

--- src/retrieval/knowledge_retriever.py
+++ src/retrieval/knowledge_retriever.py
@@ -23,29 +23,30 @@ class KnowledgeRetriever(Agent):
         
         
     def _handle_fileupload(self, topic, data):
-        this = self
-        pcl = Parcel.from_bytes(data)
-        # mdata = pcl.managed_data
-        # logger.debug(f"topic: {topic}, filename: {mdata.get('filename')}")
+        home_topic=f'{self.tag}-{int(time.time()*1000)}/{topic}'        
+        data_uploaded = self._publish_sync('FileUpload/FileService/Services', data, home_topic)
+        mdata_uploaded = Parcel.from_text(data_uploaded).managed_data
+        logger.debug(f"topic: {topic}, filename: {mdata_uploaded.get('filename')}")
+        self._publish('001/Test', data_uploaded)
+        
+        
+    # def _handle_fileupload(self, topic, data):
+    #     this = self
+    #     pcl = Parcel.from_bytes(data)
+    #     # mdata = pcl.managed_data
+    #     # logger.debug(f"topic: {topic}, filename: {mdata.get('filename')}")
 
-        def handle_file_saved(_, data_uploaded):
-            mdata_uploaded = Parcel.from_text(data_uploaded).managed_data
-            logger.debug(f"topic: {topic}, filename: {mdata_uploaded.get('filename')}")
-            this._publish('001/Test', data_uploaded)
+    #     def handle_file_saved(_, data_uploaded):
+    #         mdata_uploaded = Parcel.from_text(data_uploaded).managed_data
+    #         logger.debug(f"topic: {topic}, filename: {mdata_uploaded.get('filename')}")
+    #         this._publish('001/Test', data_uploaded)
 
-        home_topic=f'{self.tag}-{int(time.time()*1000)}/{topic}'
-        self._subscribe(home_topic, topic_handler=handle_file_saved)
+    #     home_topic=f'{self.tag}-{int(time.time()*1000)}/{topic}'
+    #     self._subscribe(home_topic, topic_handler=handle_file_saved)
         
-        # pcl = Parcel(content=mdata['content'], home_topic=home_topic)
-        pcl.set('home_topic', home_topic)
-        self._publish('FileUpload/FileService/Services', pcl.payload())
-        
-        
-        # file_info = pickle.loads(payload)
-        # if not 'file_id' in file_info:
-        #     file_info['file_id'] = ""
-        # filename = file_info['filename']
-        # content = file_info['content']
+    #     # pcl = Parcel(content=mdata['content'], home_topic=home_topic)
+    #     pcl.set('home_topic', home_topic)
+    #     self._publish('FileUpload/FileService/Services', pcl.payload())
         
 
 
---
+++ unit_test/test_publish_sync.py
@@ -0,0 +1,81 @@
+import sys
+import os
+sys.path.append(os.path.abspath(".."))  # Adjust path if necessary
+
+import time
+import unittest
+
+from agentflow.core.agent import Agent
+from agentflow.core.parcel import Parcel
+from retrieval.knowledge_retriever import KnowledgeRetriever
+from services.file_service import FileService
+from unit_test.config_test import config_test
+
+
+from logging import Logger
+logger:Logger = __import__('wastepro').get_logger()
+
+
+
+class AgentResponse(Agent):
+    def on_connected(self):
+        self._subscribe('topic_1')
+    
+    
+    def on_message(self, topic:str, data):
+        logger.debug(self.M(f"topic: {topic}, data: {data}"))
+        time.sleep(1)
+        self._publish('topic_2', int(data)+1)
+
+
+
+class TestAgent(unittest.TestCase):
+    data_resp = 0
+    data_resp_a = 0
+    data_got = False
+    
+    class ValidationAgent(Agent):
+        def __init__(self):
+            super().__init__(name='main', agent_config=config_test)
+
+
+        def on_connected(self):
+            time.sleep(1)
+            # self._publish('topic_1', 1)
+            TestAgent.data_resp = int(self._publish_sync('topic_1', 1, 'topic_2'))
+            TestAgent.data_resp_a = int(self._publish_sync('topic_1', TestAgent.data_resp, 'topic_2'))
+            TestAgent.data_got = True
+
+
+    def setUp(self):
+        self.validation_agent = TestAgent.ValidationAgent()
+        self.validation_agent.start_thread()
+        
+        self.agent_resp = AgentResponse('aaa', config_test)
+        self.agent_resp.start()
+
+
+    def _do_test_1(self):
+        self.assertTrue(TestAgent.data_got)
+        self.assertEqual(TestAgent.data_resp, 2)
+        self.assertEqual(TestAgent.data_resp_a, 3)
+
+
+    def test_1(self):
+        time.sleep(4)
+
+        try:
+            self._do_test_1()
+        except Exception as ex:
+            logger.exception(ex)
+            self.assertTrue(False)
+
+
+    def tearDown(self):
+        self.agent_resp.terminate()
+        self.validation_agent.terminate()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
