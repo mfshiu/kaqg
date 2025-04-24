f7df01fe9a5787f95461813912a27540d181ebf8
a227799770055 <a227799770055@gmail.com>
2025-04-24 22:53:43 +0800

    [Fix] Adjust random variation

--- src/generation/ranker/weighted_ranker.py
+++ src/generation/ranker/weighted_ranker.py
@@ -139,7 +139,7 @@ class ConceptScorer:
             "connected_facts": richness_result["connected_facts"]
         }
 
-    def get_concepts_average_score_and_sort(self, concept_names, alpha=0.5, a=1, b=1, c=1):
+    def get_concepts_average_score_and_sort(self, concept_names, alpha=0.5, a=1, b=1, c=1, d=0):
         all_scores = [self.get_concept_all_scores(name, alpha=alpha) for name in concept_names]
         pagerank_list = np.array([s["pagerank_score"] for s in all_scores])
         tfidf_list = np.array([s["tfidf_score"] for s in all_scores])
@@ -147,13 +147,14 @@ class ConceptScorer:
         pagerank_softmax = self.softmax(pagerank_list)
         tfidf_softmax = self.softmax(tfidf_list)
         richness_softmax = self.softmax(richness_list)
-        weight_sum = a + b + c
+        weight_sum = a + b + c 
         results = []
         for i, s in enumerate(all_scores):
             avg_score = (a * pagerank_softmax[i] + b * tfidf_softmax[i] + c * richness_softmax[i]) / weight_sum
             origin_score = avg_score.copy()
             # 隨機調整分數
-            avg_score = avg_score + (avg_score * random.uniform(-0.5, 0.5))
+            variation = random.uniform(-1, 1)*d
+            avg_score = avg_score + variation
             results.append({
                 "concept_name": s["concept_name"],
                 "pagerank_softmax": pagerank_softmax[i],
@@ -182,7 +183,7 @@ if __name__ == "__main__":
     with ConceptScorer(uri,username,password) as scorer:
         concepts = ['天氣', '降雨', '日期', '職業']
         # 例如：PageRank 權重 2，TF-IDF 權重 1，Richness 權重 1
-        sorted_results = scorer.get_concepts_average_score_and_sort(concepts, alpha=0.3, a=1, b=1, c=1)
+        sorted_results = scorer.get_concepts_average_score_and_sort(concepts, alpha=0.3, a=1, b=1, c=1, d=0.2)
         print("加權綜合分數排序：")
         for r in sorted_results:
             print(f"{r['concept_name']} → 加權平均: {r['avg_score']:.4f} | PageRank: {r['pagerank_softmax']:.4f} | TF-IDF: {r['tfidf_softmax']:.4f} | Richness: {r['richness_softmax']:.4f} | 原始分數: {r['origin_score']:.4f}")
